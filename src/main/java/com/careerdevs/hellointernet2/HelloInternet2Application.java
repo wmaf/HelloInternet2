66996d98350518e156a129223ebe41967c3b066b
wmaf <wmaf@outlook.com>
2022-04-25 13:21:18 -0400

    Initial commit HTTP API request - response project

---
+++ src/main/java/com/careerdevs/hellointernet2/HelloInternet2Application.java
@@ -0,0 +1,13 @@
+package com.careerdevs.hellointernet2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloInternet2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloInternet2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerdevs/hellointernet2/HelloInternet2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.hellointernet2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloInternet2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
